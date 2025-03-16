eaea5fb5f02cf24dfc96924b01fafb1709d9cdae
Aanandvyas <anandvyas0707@gmail.com>
2025-03-16 18:27:17 +0530

    frontpart

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Landing from './components/Landing'
 
 const App = () => {
   return (
-    <div className='bg-red-500 w-screen h-screen'>
-      <div>app</div>
+    <div className='bg-zinc-900 w-screen h-screen'>
+      <div>
+        <Navbar />
+        <Landing/>
+      </div>
     </div>
   )
 }
